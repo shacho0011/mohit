6cef20135b77e4ca79b494b0a5644eac433a4696
shacho0011 <shacho0011@gmail.com>
2019-01-29 15:16:18 +0600

    added migration file __add_user.js

--- config/migrations/20180606164349_add_user.js
+++ config/migrations/20180606164349_add_user.js
@@ -1,9 +1,12 @@
-const TABLENAME = 'Users';
+const TABLENAME = 'users';
 
 exports.up = knex => knex.schema.createTable(TABLENAME, (table) => {
   table.increments();
   table.string('name').notNullable();
-  table.string('address').notNullable();
+  table.string('email').notNullable();
+  table.string('password').notNullable();
+  table.string('phone');
+  table.string('address');
   // TODO: you need to add created_at column
   // which should be set the time when the column inserted automatically.
 });
