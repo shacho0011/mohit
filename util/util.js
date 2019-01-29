fed900c72be4edb82c185d1dcdd11c566e7473d2
shacho0011 <shacho0011@gmail.com>
2019-01-29 21:52:28 +0600

    added util.js for utility func

---
+++ util/util.js
@@ -0,0 +1,11 @@
+validateUser = user =>{
+
+    var hasName = typeof user.name == 'string' && user.name.trim() != '';
+    var hasEmail = typeof user.email == 'string' && user.email.trim() != '';
+    var hasPassword = typeof user.password == 'string' && user.password.trim() != '';
+  
+    return hasName && hasEmail && hasPassword;
+}
+
+
+module.exports = validateUser;
