1a9fd1097428b9c3cccaeece470ddc00819daed8
shacho0011 <shacho0011@gmail.com>
2019-01-30 08:54:06 +0600

    adding user and home view

---
+++ client/src/components/home.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+
+//import { register } from './userFunc'
+
+
+class Home extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            first_name: '',
+            last_name: '',
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+        
+    }
+
+    onChange (e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit (e) {
+        e.preventDefault()
+
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        // login(user).then(res => {
+        //     if (res) {
+        //         //this.props.history.push(`/profile`)
+        //     }
+        // })
+    }
+
+  render() {
+    return (
+        <div className="row">
+            <div className="col-lg-5 md-5 sm-5 xs-12 offset-1">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                            <div className="form-group">
+                                <label htmlFor="first_name">First Name</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="first_name"
+                                    placeholder="Enter First Name"
+                                    value={this.state.first_name}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="last_name">Last Name</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="last_name"
+                                    placeholder="Enter Last Name"
+                                    value={this.state.last_name}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Enter Email"
+                                    value={this.state.email}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    name="password"
+                                    placeholder="Enter Password"
+                                    value={this.state.password}
+                                    onChange={this.onChange} />
+                            </div>
+                            <button type="submit" className="btn btn-lg btn-primary btn-block">
+                                Register
+                            </button>
+                        </form>
+                    </div>
+
+                    <div className="col-lg-5 md-5 sm-5 xs-12 offset-1">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                            <div className="form-group">
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Enter Email"
+                                    value={this.state.email}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    name="password"
+                                    placeholder="Enter Password"
+                                    value={this.state.password}
+                                    onChange={this.onChange} />
+                            </div>
+                            <button type="submit" className="btn btn-lg btn-primary btn-block">
+                                Sign in
+                            </button>
+                        </form>
+                    </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ client/src/components/users.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div className="Users">
+      </div>
+    );
+  }
+}
+
+export default Users;
